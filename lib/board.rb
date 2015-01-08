c21e80a630de6203b034bb983431a35254b09d85
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-08 15:41:37 -0700

    Implemented blank_finder and blank_finder to pass into boards

---
+++ lib/board.rb
@@ -0,0 +1,34 @@
+require_relative 'number_finder'
+
+class Board
+
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def solve
+    #output = #gridify(input)
+    #output.each do
+      #number_finder(output)
+      #blank_finder(output)
+    #end
+    #index_finder?
+
+
+  end
+
+end
+
+
+# board = Board.new('826594317
+# 715638942
+# 394721865
+# 163459278
+# 948267153
+# 257813694
+# 531942786
+# 482176539
+# 679385421')
+# board.grid
